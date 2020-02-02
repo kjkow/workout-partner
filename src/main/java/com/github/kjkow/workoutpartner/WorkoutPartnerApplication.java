84cf32f940e0e75dac54d0589f14d71ae55cc84f
kjkow <kamil.kowalczyk992@gmail.com>
2020-02-02 06:24:45 +0100

    init repo

---
+++ src/main/java/com/github/kjkow/workoutpartner/WorkoutPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kjkow.workoutpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPartnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kjkow/workoutpartner/WorkoutPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kjkow.workoutpartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
