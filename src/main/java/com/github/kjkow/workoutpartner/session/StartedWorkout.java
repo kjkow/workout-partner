6f5f2d3d0319e9bd03a0784755062e6c6aa85ee7
kjkow <kamil.kowalczyk992@gmail.com>
2020-04-09 20:19:55 +0200

    exercise started

--- src/main/java/com/github/kjkow/workoutpartner/session/StartedWorkout.java
+++ src/main/java/com/github/kjkow/workoutpartner/session/StartedWorkout.java
@@ -3,20 +3,17 @@ package com.github.kjkow.workoutpartner.session;
 import com.github.kjkow.workoutpartner.commons.Events;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 class StartedWorkout {
-    @NonNull
-    private final UUID workoutId;
 
+    private final UUID workoutId;
     private final Events events;
 
-
-    public void startExercise(String exercise) {
+    void startExercise(String exercise) {
         //TODO is on the list?
         events.publish(new ExerciseStarted(workoutId, Instant.now()));
     }
--- src/test/java/com/github/kjkow/workoutpartner/session/StartedWorkoutTest.java
+++ src/test/java/com/github/kjkow/workoutpartner/session/StartedWorkoutTest.java
@@ -7,23 +7,57 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.noInteractions;
 
 class StartedWorkoutTest {
 
     private final Events events = mock(Events.class);
 
-    @DisplayName("") //todo description
+    private StartedWorkout workout;
+    private String exercise;
+
+    @DisplayName("given started workout, should start exercise")
     @Test
     void testStartExercise() {
         //given
-        var startedWorkout = new StartedWorkout(UUID.randomUUID(), events);
-        var deadLiftExercise = "deadlift";
+        startedWorkout();
+        //and
+        startingExerciseOnTheList();
 
         //when
-        startedWorkout.startExercise(deadLiftExercise);
+        workout.startExercise(exercise);
 
         //then
         verify(events, times(1)).publish(any(ExerciseStarted.class));
+    }
+
+    @DisplayName("given started workout, " +
+            "when starting exercise that is not on the plan, " +
+            "should reject adding exercise")
+    @Test
+    void testStartExerciseNotOnPlan() {
+        //given
+        startedWorkout();
+        //and
+        startingExerciseNotOnPlan();
+
+        //when
+        workout.startExercise(exercise);
+
+        //then
+        verify(events, noInteractions()).publish(any()); //TODO sth else than no interactions? Rejected event?
+    }
+
+    private void startingExerciseNotOnPlan() {
+        exercise = "bench press";
+    }
+
+    private void startingExerciseOnTheList() {
+        exercise = "deadlift";
+    }
+
+    private void startedWorkout() {
+        workout = new StartedWorkout(UUID.randomUUID(), events);
     }
 
 }
